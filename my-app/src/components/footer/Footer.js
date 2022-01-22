9cdbc9af89c14e95386d471a919112f59d8afd05
Jakhongir <iskandarovjahongir@gmail.com>
2022-01-22 22:36:44 +0900

    updated

--- my-app/src/components/footer/Footer.js
+++ my-app/src/components/footer/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './footer.scss'
 import { FaGithubSquare} from "react-icons/fa"
-import {SiLinkedin, SiInstagram, SiNetlify} from "react-icons/si";
+import {SiLinkedin,SiTwitter, SiInstagram, SiNetlify} from "react-icons/si";
 
 
 function Footer() {
@@ -11,6 +11,7 @@ function Footer() {
         <a className='in-icon'  href='https://www.linkedin.com/in/jahongir-iskandarov-a5286b105/'><SiLinkedin/> </a>
         <a className='netlify-icon'  href='https://app.netlify.com/teams/jahon93/overview?_ga=2.73564818.1641667257.1642831016-749102175.1638703153'><SiNetlify /> </a>
         <a className='instagram-icon'  href='https://www.instagram.com/jakhongir_iskandarov/'><SiInstagram /> </a>
+        <a className='twitter-icon'  href='https://twitter.com/jahongiriy'><SiTwitter/> </a>
         
         
         </div>
