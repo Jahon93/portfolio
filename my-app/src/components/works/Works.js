23a1a5fd4ec8dac6aa89b5041c991b551dd4e0e9
Jakhongir <iskandarovjahongir@gmail.com>
2022-01-23 22:47:55 +0900

    updated

--- my-app/src/components/works/Works.js
+++ my-app/src/components/works/Works.js
@@ -4,6 +4,7 @@ import img1 from './images/img1.PNG'
 import img2 from './images/img2.PNG'
 import img3 from './images/img3.PNG'
 import img4 from './images/img4.PNG'
+import adviserBot from './images/adviserBot.PNG'
 
 export default function Works() {
   return (
@@ -23,8 +24,8 @@ export default function Works() {
           <img src={img2} alt=""/>
           <h2 className='items-name'>JavaScript Tutorial</h2>
           <ul>
-            <li><a href='##'>View Codee</a></li>
-            <li><a href='##'>See Project</a></li>
+            <li><a href='https://github.com/Jahon93/Javascript-documentation'>View Codee</a></li>
+            <li><a href='https://javascript-documentation1.netlify.app/'>See Project</a></li>
           </ul>          
         </div> 
 
@@ -32,26 +33,26 @@ export default function Works() {
           <img src={img3} alt=""/>
           <h2 className='items-name'>Survey Form</h2>
           <ul>
-            <li><a href='##'>View Codee</a></li>
-            <li><a href='##'>See Project</a></li>
+            <li><a href='https://github.com/Jahon93/Survey-form'>View Codee</a></li>
+            <li><a href='https://surveyindeveleopment.netlify.app/'>See Project</a></li>
           </ul>          
         </div>  
 
         <div className='works-items'>
           <img src={img4} alt=""/>
-          <h2 className='items-name'>People Counter</h2>
+          <h2 className='items-name'>Passanger Counter</h2>
           <ul>
-            <li><a href='##'>View Code</a></li>
-            <li><a href='##'>See Project</a></li>
+            <li><a href='https://github.com/Jahon93/passenger-counter'>View Code</a></li>
+            <li><a href='https://bus-passenger-counter.netlify.app/'>See Project</a></li>
           </ul>          
         </div>  
 
         <div className='works-items'>
-          <img src={img1} alt=""/>
-          <h2 className='items-name'>x project</h2>
+          <img src={adviserBot} alt=""/>
+          <h2 className='items-name'>Adviser Bot</h2>
           <ul>
-            <li><a href='##'>View Codee</a></li>
-            <li><a href='##'>See Project</a></li>
+            <li><a href='https://github.com/Jahon93/Smart-Bot'>View Code</a></li>
+            <li><a href='https://smart-bot.netlify.app/'>See Project</a></li>
           </ul>          
         </div> 
 
@@ -59,7 +60,7 @@ export default function Works() {
           <img className='netflix-img' src="https://www.whats-on-netflix.com/wp-content/uploads/2021/12/coming-to-netflix-this-week-december-20th-to-26th-2021.png"
            alt=""/>
           <h2 className='items-name netflix-name'>Netflix Clone</h2>
-          <p className='soon-text'>soon...</p>        
+          <p className='soon-text'>Coming soon...</p>        
         </div>  
 
       </div>
